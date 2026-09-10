d5776bd730ceb20b0810ae2ce4893efaabef0a08
Antony Marshal <96686984+Antonymarshal24@users.noreply.github.com>
2026-09-10 14:33:52 +0530

    validation message change

--- auth/src/company/routes/signup.ts
+++ auth/src/company/routes/signup.ts
@@ -12,7 +12,7 @@ const router = express.Router();
 router.post(
   "/api/users/signup",
   [
-    body("email").isEmail().withMessage("Email must be valid"),
+    body("email").isEmail().withMessage("Must be valid email"),
     body("password").trim().isLength({ min: 4, max: 20 }).withMessage("Password must be between 4 and 20 characters"),
   ],
   validateRequest,
