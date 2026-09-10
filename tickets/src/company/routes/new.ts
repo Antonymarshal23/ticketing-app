d465e95bfe15df66345cfa4ed1a18712fa8fddf7
Antony Marshal <96686984+Antonymarshal24@users.noreply.github.com>
2026-09-10 14:42:53 +0530

    all service requuest validation chnaged

--- tickets/src/company/routes/new.ts
+++ tickets/src/company/routes/new.ts
@@ -13,7 +13,7 @@ router.post(
   '/api/tickets',
   requireAuth("company", User),
   [
-    body('title').not().isEmpty().withMessage('Title is required'),
+    body('title').not().isEmpty().withMessage('Title is required.'),
     body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0')
   ],
   validateRequest,
